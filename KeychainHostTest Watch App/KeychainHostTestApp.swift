ad8e648308dc73d6189072d70077ee61c588e18c
Grant Espanet <gespanet97@gmail.com>
2024-09-20 04:36:36 -0400

    PurLogWatch setup

---
+++ KeychainHostTest Watch App/KeychainHostTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainHostTestApp.swift
+//  KeychainHostTest Watch App
+//
+//  Created by Grant Espanet on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct KeychainHostTest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PurLogWatch/PurLogWatch.h
@@ -0,0 +1,18 @@
+//
+//  PurLogWatch.h
+//  PurLogWatch
+//
+//  Created by Grant Espanet on 9/20/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PurLogWatch.
+FOUNDATION_EXPORT double PurLogWatchVersionNumber;
+
+//! Project version string for PurLogWatch.
+FOUNDATION_EXPORT const unsigned char PurLogWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurLogWatch/PublicHeader.h>
+
+
